1ff48befb0477510a9c3efd80d0fef42485e2f16
Dominguez Gastón Matías <gastonmd14@gmail.com>
2021-03-26 19:24:52 -0300

    Se entrega por la ruta /api los registros y balance de un usuario

--- controllers/api/indexController.js
+++ controllers/api/indexController.js
@@ -1,7 +1,30 @@
+const db = require("../../database/models");
 
 module.exports = {
-  
   home: (req, res, next) => {
-      
-  }
-}
+    db.Record.findAll({
+      include: [
+        {
+          association: "balances",
+          where: {
+            users_id: 1,
+          },
+        },
+      ],
+      limit: 10,
+      order: ["date"],
+    })
+    .then((r) => {
+      let responsd = {
+        meta: {
+          status: 200,
+          total: r.lenght,
+          url: "/api/",
+        },
+        data: r,
+      };
+
+      res.json(responsd);
+    });
+  },
+};
